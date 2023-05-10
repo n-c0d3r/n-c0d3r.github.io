c00cf9789d332c16852f2c6686940aa8aceaf60b
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-05-10 17:47:18 +0700

    update ncpp docs

--- project_docs/ncpp/internal/ncpp/html/is__loggable_8hpp.js
+++
@@ -1,4 +0,0 @@
-var is__loggable_8hpp =
-[
-    [ "ncpp::utilities::is_loggable_t< type__ >", "structncpp_1_1utilities_1_1is__loggable__t.html", null ]
-];
--- project_docs/ncpp/internal/ncpp/html/search/all_14.js
+++ project_docs/ncpp/internal/ncpp/html/search/all_14.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['var_5ft_0',['var_t',['../classncpp_1_1rtti_1_1robject__i.html#a25c4693e6eddeb9916d58f82fdfb0b08',1,'ncpp::rtti::robject_i::var_t(const containers::native_string &amp;var_name)'],['../classncpp_1_1rtti_1_1robject__i.html#a2a5aabf393e915fc504282ff41594d49',1,'ncpp::rtti::robject_i::var_t(const containers::native_string &amp;var_name) const']]]
+  ['var_5ft_0',['var_t',['../classncpp_1_1rtti_1_1robject__i.html#af9d6c44d4127e056d04d0ecdd171c077',1,'ncpp::rtti::robject_i::var_t(const containers::native_string &amp;member_name)'],['../classncpp_1_1rtti_1_1robject__i.html#a468399861e4d1028f43ec4c28c354c0f',1,'ncpp::rtti::robject_i::var_t(const containers::native_string &amp;member_name) const']]]
 ];
