41b8a028def67e65209b03926fd7db0501897669
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-07-22 16:38:24 +0700

    Update header_menu_container_mobile.js

--- source/scripts/app/ui/header_menu_container_mobile.js
+++ source/scripts/app/ui/header_menu_container_mobile.js
@@ -161,14 +161,10 @@ export default function HeaderMenuContainerMobile(){
                 headerMenuContainer.isTouchMove = true;
             });
 
-            function isInViewport(element) {
-                const rect = element.getBoundingClientRect();
-                return (
-                    rect.top >= 0 &&
-                    rect.left >= 0 &&
-                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-                );
+            function checkVisible(elm) {
+                var rect = elm.getBoundingClientRect();
+                var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+                return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
             }
 
             window.addEventListener("touchend", function(){
@@ -176,7 +172,7 @@ export default function HeaderMenuContainerMobile(){
                 if(
                     headerMenuContainer.state != "hidden"
                     && headerMenuContainer.isTouchMove
-                    && !isInViewport(headerMenuContainer)
+                    && !checkVisible(headerMenuContainer)
                 )
                     headerMenuContainer.hide();
 
