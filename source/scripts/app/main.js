bc2ab50d67d64b1c965aad62e33b39d551878cdb
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-08-18 23:06:00 +0700

    remake all

--- source/scripts/app/main.js
+++
@@ -1,9 +0,0 @@
-import framework from "../framework/framework.js"
-
-import InitWeb from "./animation/init_web.js"
-
-
-
-framework.init();
-
-await InitWeb();
--- source/scripts/app/ui/page_content.js
+++
@@ -1,9 +0,0 @@
-import framework from "../../framework/framework.js"
-
-
-
-export default function PageContent(pageName){
-
-    return framework.ScriptModule(`${window.location.origin}/source/scripts/app/ui/${pageName}/${pageName}.js`);
-
-}
